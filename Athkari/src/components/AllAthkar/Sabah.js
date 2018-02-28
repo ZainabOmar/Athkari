07cd3c0855900a331d9146ca8578469521a0b69a
zainab <zainab.hammami.zh@gmail.com>
2018-03-01 00:02:17 +0200

    adding more data and fetch it to screen

--- Athkari/src/components/AllAthkar/Sabah.js
+++ Athkari/src/components/AllAthkar/Sabah.js
@@ -4,6 +4,7 @@ import {
   StatusBar,
   View,
   Text,
+  TouchableOpacity,
 } from 'react-native';
 // import { fetch } from 'isomorphic-fetch';
 
@@ -17,6 +18,23 @@ export default class Sabah extends Component<Props> {
     this.setState({ myData: data });
   }
 
+  renderAthkar() {
+    return this.state.myData.map(
+      thikr =>
+      <View>
+        <View style={{ alignItems: 'flex-end', }}>
+          <TouchableOpacity style={styles.button}>
+            <Text key={thikr.count} style={styles.buttonText}>{thikr.count}</Text>
+          </TouchableOpacity>
+          <Text key={thikr.title} style={styles.text}>
+            {thikr.title}
+            {'\n'}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
   render() {
     console.log('here is the data', this.state);
     return (
@@ -26,6 +44,7 @@ export default class Sabah extends Component<Props> {
           barStyle='light-content'
         />
         <Text style={styles.header}>أذكار الصباح</Text>
+        {this.renderAthkar()}
       </View>
     );
   }
@@ -40,5 +59,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#8c7b75',
     padding: 20,
     marginBottom: 20
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: 'black'
+  },
+  button: {
+    alignItems: 'center',
+    borderColor: '#321911',
+    width: 90,
+    paddingVertical: 10,
+    backgroundColor: '#d7ccc8',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#321911',
+    fontSize: 20,
+    fontWeight: '500',
   }
 });
